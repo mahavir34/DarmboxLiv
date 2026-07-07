98b6a11aab0ce06c67ea48d2fcd8353f39616f42
mahavir34 <mahavirbeniwal538@gmail.com>
2026-07-07 22:22:21 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -23,8 +23,27 @@ module.exports = async (req, res) => {
         if (epsFuncKey && typeof scraper[epsFuncKey] === 'function') {
             const episodes = await scraper[epsFuncKey](dramaUrl);
             
-            // यहाँ हम कोई फ़िल्टर नहीं लगाएँगे, सीधा पूरा डेटा फ्रंटएंड को भेज देंगे ताकि हम उसकी चाबियाँ (Keys) देख सकें
-            return res.status(200).json({ success: true, debug_data: episodes });
+            let finalUrl = null;
+
+            // स्मार्ट डिक्शनरी डिटेक्शन लॉजिक
+            if (episodes) {
+                // स्थिति 1: अगर एरे (Array) के रूप में डेटा आया हो
+                if (Array.isArray(episodes) && episodes.length > 0) {
+                    let ep = episodes[0];
+                    finalUrl = ep.video_url || ep.play_url || ep.current_episode_mp4_url || ep.url;
+                } 
+                // स्थिति 2: अगर सीधा ऑब्जेक्ट (Object) हो
+                else {
+                    finalUrl = episodes.video_url || episodes.play_url || episodes.current_episode_mp4_url || episodes.url;
+                }
+            }
+
+            // अगर सीधे यूआरएल मिल गया तो वेल एंड गुड, नहीं तो पूरा ऑब्जेक्ट फ्रंटएंड को डिबग के लिए दे दो
+            if (finalUrl) {
+                return res.status(200).json({ success: true, video_url: finalUrl.replace(/\\/g, '') });
+            } else {
+                return res.status(200).json({ success: true, video_url: null, raw_response: episodes });
+            }
         } else {
             return res.status(500).json({ success: false, error: 'स्क्रैपर फंक्शन लोड नहीं हो पाया।' });
         }
